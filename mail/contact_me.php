0dca047b333d943aaf86a239bfdf93bbdb8075f6
Maureen Slavin <maureen.slavin@q2ebanking.com>
2018-08-05 08:11:35 -0500

    final draft: removing commented out code, replaced thumbnail images in timeline, added company field to contact form

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,6 +1,7 @@
 <?php
 // Check for empty fields
 if(empty($_POST['name'])      ||
+   empty($_POST['company'])   ||
    empty($_POST['email'])     ||
    empty($_POST['phone'])     ||
    empty($_POST['message'])   ||
@@ -11,6 +12,7 @@ if(empty($_POST['name'])      ||
    }
    
 $name = strip_tags(htmlspecialchars($_POST['name']));
+company = strip_tags(htmlspecialchars($_POST['company']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
@@ -18,7 +20,7 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 // Create the email and send the message
 $to = 'info@bbtechgrp.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nCompany: $company\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@bbtechgrp.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
